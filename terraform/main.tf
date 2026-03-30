4475307d00217c11fd96067713d3d7cd254b7f4b
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2026-03-30 23:18:44 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,14 +55,25 @@ resource "aws_instance" "lab6_server" {
 
               while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do sleep 5; done
               
-              apt-get update -y
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              
-              apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-              
+              sudo apt update
+              sudo apt install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+                    Types: deb
+                    URIs: https://download.docker.com/linux/ubuntu
+                    Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+                    Components: stable
+                    Architectures: $(dpkg --print-architecture)
+                    Signed-By: /etc/apt/keyrings/docker.asc
+              EOF
+
+              sudo apt update
+              sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+              sudo apt-get install docker-compose-plugin
+
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
