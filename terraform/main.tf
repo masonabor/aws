a652c8cdbac04e8a110f7a7a928a073ff78a35d6
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2026-03-30 23:57:21 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,17 +61,11 @@ while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
 done
 
 apt-get update -y
-apt-get install -y ca-certificates curl gnupg lsb-release
-
-install -m 0755 -d /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-
 apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker.io
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
 
 systemctl enable docker
 systemctl start docker
